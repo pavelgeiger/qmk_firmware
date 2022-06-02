776463b3f9f983db2cd9307a63d2d1726f9f0e3b
Pavel Geiger <geiger@jablotron.cz>
2022-06-02 13:00:06 +0200

    Use caps word

--- keyboards/preonic/keymaps/moje/keymap.c
+++ keyboards/preonic/keymaps/moje/keymap.c
@@ -47,19 +47,19 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_preonic_1x2uC(
-        KC_ESC,              KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,     KC_8,    KC_9,   KC_0,    KC_BSPC, \
-        KC_MEDIA_PLAY_PAUSE, KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,    RALT(KC_QUOT),
-        KC_TAB,              KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        TD(TD_LSHIFT_CAPS),  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
-        KC_LCTL,             KC_LGUI, KC_LGUI, KC_LALT, MO(UP),   SPACE_FN,    MO(DOWN), KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT
+        KC_ESC,              KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC, \
+        KC_MEDIA_PLAY_PAUSE, KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    RALT(KC_QUOT),
+        KC_TAB,              KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+        KC_LCTL,             KC_LGUI, KC_LGUI, KC_LALT, MO(UP),   SPACE_FN,    MO(DOWN), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
     [UP] = LAYOUT_preonic_1x2uC(
-        KC_TILD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQUAL,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
+        KC_TILD,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, \
+        XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQUAL,
+        KC_F1,              KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        TD(TD_LSHIFT_CAPS), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______,            _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
     ),
 
     [DOWN] = LAYOUT_preonic_1x2uC(
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PSCR, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   XXXXXXX,
         _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   XXXXXXX,
         _______, XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,   _______,
-        RESET,   MU_TOG,     CK_TOGG,    XXXXXXX,     XXXXXXX,      _______,     XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP, XXXXXXX
+        QK_BOOT, MU_TOG,     CK_TOGG,    XXXXXXX,     XXXXXXX,      _______,     XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP, XXXXXXX
     ),
 
     [UP_DOWN] = LAYOUT_preonic_1x2uC(
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-        KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, _______,      _______,     _______, _______, CK_UP, CK_DOWN, _______
+        KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, _______,      _______,     _______, _______, CK_UP,   CK_DOWN, _______
     )
 };
 
@@ -115,6 +115,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT)); // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_SLSH:
+        case KC_LSFT:
+            return true;
+
+        default:
+            return false; // Deactivate Caps Word.
     }
 }
 
